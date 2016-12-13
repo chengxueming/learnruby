7259c26f35301f248ec2d0cf3addb2deb5cd8e99
chengxueming <=>
2016-12-13 21:17:35 +0800

    添加魔方旋转任意一个面

--- main.rb
+++ main.rb
@@ -1,2 +1,14 @@
-caller $stdout.flush
-puts "hello"
+#puts [1,2,3,4,5,6].reverse!
+# l = [5,2,3]
+# l.each{|a|puts a
+# }
+# if l[0] > 2;puts "hello";end
+#puts[0:]
+  index = 0
+  v = [1,0,0]
+  #v.each{|single| if single != 0;index =i;i=i +1;end}
+  while index < v.length
+  	if v[index] != 0;break;end
+  	index=index +1
+  end
+  puts index
